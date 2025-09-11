f7f3650812a3356d562c53b401dc5e523a560bb0
dper1977 <dper1977@gmail.com>
2025-09-11 12:51:30 +0700

    update 4

---
+++ JavaScript/Basic JS/test.js
@@ -0,0 +1,13 @@
+<body></body>;
+
+let Hellotext;
+Hellotext = "Hallo Bumi";
+Cosule.log(Hellotext);
+
+let usia = 24;
+console.log(usia);
+
+const namaBelakang = "Santoso";
+console.log(namaBelakang);
+
+// mencoba
---
+++ boolean_script.js
@@ -0,0 +1,6 @@
+let saved;
+saved = true;
+const done = false;
+
+console.log("saved:", saved);
+console.log("done:", done);
